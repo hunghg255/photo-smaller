c15abe7984c0ecb9f084c8dd17c147cdb5e5526b
hunghg255 <giahunghust@gmail.com>
2024-05-30 22:30:38 +0700

    chore: add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA()],
   base: "/",
   resolve: {
     alias: {
